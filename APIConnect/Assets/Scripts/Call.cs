b1f7e31f7ba494a004d25654bdd6f30c514a1471
Aishwary Pramanik <aishwarypramanik@gmail.com>
2017-10-31 20:52:28 -0400

    Basic Functionality

---
+++ APIConnect/Assets/Scripts/Call.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+public class Call : MonoBehaviour {
+
+    private string url = "http://pokeapi.co/api/v2/pokemon/";
+    private JsonData data;
+    IEnumerator Start()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        data = JsonMapper.ToObject(www.text);
+        Debug.Log(data["results"][0][1]);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ APIConnect/Assets/Scripts/Request.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Request : MonoBehaviour {
+    public string url = "https://upload.wikimedia.org/wikipedia/en/thumb/0/09/AshXYanime.png/250px-AshXYanime.png";
+
+    // Use this for initialization
+    void Start () {
+        WWW www = new WWW(url);
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = www.texture;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
